2ce99f7310316329f1c8693f0fdd987a3476122f
jullia <contato.julliaizidorio@gmail.com>
2023-05-08 07:46:31 -0300

    instalando libs e criando pastas e arquivos

--- src/data/insertUser.ts
+++
@@ -1,9 +0,0 @@
-import { connection } from "..";
-
-export default async function insertUser(name: string, nickname: string, email: string) {
-
-    await connection.insert({
-        name, nickname, email
-    }).into("todolistuser")
-
-}
