24409fb4264361f4bc7e949a67a31582a759c8f0
jullia <contato.julliaizidorio@gmail.com>
2023-05-08 09:29:03 -0300

    ajustando endpoint de get users para aceitar busca específica com query

--- src/endpoints/getUsers.ts
+++ src/endpoints/getUsers.ts
@@ -3,8 +3,18 @@ import { connection } from "../connection"
 
 export async function getUsers(req: Request, res: Response) {
     try {
+        const id = req.query.user
+
         const result = await connection("todolistuser")
-            .select()
+        .select().where(id ? { id } : {})
+
+        // const query = connection("todolistuser").select()
+        // if(id){
+        //     query.where({id})
+        // }
+
+        // const result = await query         
+       
 
         res.send(result).status(200)
     } catch (error) {
